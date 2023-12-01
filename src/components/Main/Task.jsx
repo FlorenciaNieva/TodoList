bc327d6079a3446ec3b07560723a407240d614ba
FlorenciaNieva <florencianieva930@gmail.com>
2023-12-01 15:46:24 -0300

    Agregó formulario para añadir tareas

--- src/components/Main/Task.jsx
+++ src/components/Main/Task.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FormControl, FormLabel, FormErrorMessage, Input, Button} from '@chakra-ui/react'
+import { Form, Field, Formik } from 'formik';
+import { FaAngleRight } from "react-icons/fa";
 
 export default function Task() {
   function validateTask(value) {
@@ -15,7 +18,42 @@ export default function Task() {
     return error
     }
 
-  return (
-    <div>Task</div>
-  )
+    return (
+      <Formik
+        initialValues={{
+          task: '',
+        }}
+        onSubmit={(values, actions) => {
+          setTimeout(() => {
+            alert('se agrego la tarea')
+            actions.setSubmitting(false)
+          }, 500)
+        }}
+        >
+        {(props) => (
+          <Form>
+            <Field name='task' validate={validateTask}>
+              {({ field, form }) => (
+                <FormControl isInvalid={form.errors.task} isRequired>
+                  <FormLabel>Task</FormLabel>
+                  <Input {...field} placeholder='Escribe una tarea ...' />
+                  <FormErrorMessage>{form.errors.task}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+            <Button
+              mt={4}
+              rightIcon={<FaAngleRight />}
+              colorScheme='teal'
+              variant='outline'
+              isLoading={props.isSubmitting}
+              type='submit'
+            >
+              Add task
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    )
+  
 }

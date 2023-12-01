86615cc83bf4b949d072900e8bf6658ea6ec819c
FlorenciaNieva <florencianieva930@gmail.com>
2023-12-01 16:28:58 -0300

    Creo estado para las tareas y como valor inicial le pido que traiga los valores del localStorage o que comience con un array vacio

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css'
 import Header from './Components/Header/Header'
 import Main from './components/Main/Main'
+import { useState } from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState(localStorage.getItem('tasks') || [])
+  
   return (
     <>
       <Header />

7e0aa90f21a51f6a3fb8e0bdf43a285bd221d5e0
FlorenciaNieva <florencianieva930@gmail.com>
2023-12-01 15:18:39 -0300

    Agregó estructura flex para los formularios

--- src/components/Main/ContainerForm.jsx
+++ src/components/Main/ContainerForm.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Flex, Spacer, Box} from '@chakra-ui/react'
 
 export default function ContainerForm() {
   return (
-    <div>ContainerForm</div>
+    <>
+      <Flex>
+        <Box m='4' p='10' w='50%'>
+          Box 1
+        </Box>
+        <Spacer />
+        <Box m='4' p='10' w='50%'>
+          Box 2
+        </Box>
+      </Flex>
+    </>
   )
 }

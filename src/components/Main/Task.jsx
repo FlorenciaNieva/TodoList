8981f1f3ffcfa04cad94f0da6291d193eadc1654
FlorenciaNieva <florencianieva930@gmail.com>
2023-12-01 15:39:39 -0300

    Creo función para validar el input de las tareas

--- src/components/Main/Task.jsx
+++ src/components/Main/Task.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
 
 export default function Task() {
+  function validateTask(value) {
+    let error
+    if (!value) {
+      error = 'Escribe una tarea'
+    } else if (/\d/.test(value)) {
+      error  = "El campo no permite números"
+    } else if (value.length < 4) {
+      error  = "El campo debe contener al menos 4 caracteres"
+    } else if (value.length > 100) {
+      error = "El campo no puede exceder los 100 caracteres"
+    } 
+    return error
+    }
+
   return (
     <div>Task</div>
   )

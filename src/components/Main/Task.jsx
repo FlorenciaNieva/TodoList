053c7de444cf4943193e250653e8549b7c84bb60
FlorenciaNieva <florencianieva930@gmail.com>
2023-12-02 20:38:10 -0300

    Arreglo el lugar del > en el botón de completado

--- src/components/Main/Task.jsx
+++ src/components/Main/Task.jsx
@@ -27,7 +27,7 @@ export default function Task({ task, id, onDelete }) {
       <Flex justifyContent="space-between" alignItems="center">
         <Text>{task}</Text>
         <Flex>
-          <Button mr={2} colorScheme='whatsapp' variant='solid' aria-label="Completado"> onClick={() => toggleComplete()}<TiTickOutline /></Button>
+          <Button mr={2} colorScheme='whatsapp' variant='solid' aria-label="Completado" onClick={() => toggleComplete()}><TiTickOutline /></Button>
           <Button colorScheme='red' variant='outline' aria-label="Eliminar" onClick={() => onDelete(id)}><TiTimesOutline /></Button>
         </Flex>
       </Flex>

1e1f52051c212c08a5de07621cf17613dc89d80d
FlorenciaNieva <florencianieva930@gmail.com>
2023-12-02 20:31:43 -0300

    Agregó evento onClick a el botón de completado con función toggleComplete

--- src/components/Main/Task.jsx
+++ src/components/Main/Task.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { Flex, Box, Button, Text } from '@chakra-ui/react'
 import { TiTickOutline } from "react-icons/ti";
 import { TiTimesOutline } from "react-icons/ti";
-import styles from "./Task.module.css"
 
 export default function Task({ task, id, onDelete }) {
   const [complete, setComplete] = useState(false);
@@ -25,7 +24,7 @@ export default function Task({ task, id, onDelete }) {
       <Flex justifyContent="space-between" alignItems="center">
         <Text>{task}</Text>
         <Flex>
-          <Button mr={2} colorScheme='whatsapp' variant='solid' aria-label="Completado"><TiTickOutline /></Button>
+          <Button mr={2} colorScheme='whatsapp' variant='solid' aria-label="Completado"> onClick={() => toggleComplete()}<TiTickOutline /></Button>
           <Button colorScheme='red' variant='outline' aria-label="Eliminar" onClick={() => onDelete(id)}><TiTimesOutline /></Button>
         </Flex>
       </Flex>

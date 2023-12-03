8f0dff9baae334ecf45ab37d7554291a3280fdf7
FlorenciaNieva <florencianieva930@gmail.com>
2023-12-03 17:35:51 -0300

    Agregó bordes y fondo desenfocado en los formularios

--- src/components/Main/Task.jsx
+++ src/components/Main/Task.jsx
@@ -11,18 +11,21 @@ export default function Task({ task, id, complete, onDelete, setCompleted }) {
 
   return (
     <Box borderWidth="1px"
+      bg='#f6f1ff'
       borderRadius="lg"
       overflow="hidden"
-      p="4"
+      p="3"
       boxShadow="md"
       mb="4"
+      maxW='85%'
+      mx="auto"
       className={complete ? styles.complete : ''}
       >
       <Flex justifyContent="space-between" alignItems="center">
-        <Text>{task}</Text>
+        <Text fontSize='lg'>{task}</Text>
         <Flex>
-          <Button mr={2} colorScheme='whatsapp' variant='solid' aria-label="Completado" onClick={() => handleComplete(task, id, complete)}><TiTickOutline /></Button>
-          <Button colorScheme='red' variant='outline' aria-label="Eliminar" onClick={() => onDelete(id)}><TiTimesOutline /></Button>
+          <Button mr={2} bg='#9944ff' color='#f6f1ff' variant='outline' aria-label="Completado" onClick={() => handleComplete(task, id, complete)}><TiTickOutline /></Button>
+          <Button bg='#6e0acd' color='#f6f1ff' variant='outline' aria-label="Eliminar" onClick={() => onDelete(id)}><TiTimesOutline /></Button>
         </Flex>
       </Flex>
     </Box>

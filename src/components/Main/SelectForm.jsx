c47f6e106531ed96d5321b5064dece7d1a8d8477
FlorenciaNieva <florencianieva930@gmail.com>
2023-12-03 16:44:37 -0300

    Cambio de color de el label y el select a un blanco

--- src/components/Main/SelectForm.jsx
+++ src/components/Main/SelectForm.jsx
@@ -6,8 +6,8 @@ export default function SelectForm({ filterTasks }) {
   return (
     <div>
       <FormControl>
-        <FormLabel>Mostrar ...</FormLabel>
-        <SelectChakra onChange={(e) => filterTasks(e.target.value)}>
+        <FormLabel color='#f6f1ff'>Mostrar ...</FormLabel>
+        <SelectChakra color='#f6f1ff' onChange={(e) => filterTasks(e.target.value)}>
           <option value='todos'>Todos</option>
           <option value='completas'>Completas</option>
           <option value='incompletas'>Incompletas</option>

04c95c6488185e7ab4e83dc15ad8e91042eeb30b
FlorenciaNieva <florencianieva930@gmail.com>
2023-12-03 16:30:43 -0300

    Cambio de color del título

--- src/components/Header/Title.jsx
+++ src/components/Header/Title.jsx
@@ -4,7 +4,7 @@ import { Heading } from '@chakra-ui/react'
 export default function Title() {
   return (
     <>
-      <Heading as='h1' size='4xl'>
+      <Heading as='h1' size='4xl' color='#f6f1ff'>
         Todo List
       </Heading>
     </>

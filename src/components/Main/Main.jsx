2f244f0e20e179e94090bb1f49536947332d5e67
FlorenciaNieva <florencianieva930@gmail.com>
2023-12-03 14:53:10 -0300

    Desestructuro el prop setCompleted y lo paso a el componente ContainerList

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ContainerForm from './ContainerForm'
 import ContainerLists from './ContainerLists'
 
-export default function Main({ tasks, setTasks, onDelete }) {
+export default function Main({ tasks, setTasks, onDelete, setCompleted }) {
   return (
     <main>
       <ContainerForm tasks={tasks} setTasks={setTasks}/>
-      <ContainerLists tasks={tasks} onDelete={onDelete}/>
+      <ContainerLists tasks={tasks} onDelete={onDelete} setCompleted={setCompleted} />
     </main>
   )
 }

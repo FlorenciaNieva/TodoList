a2363ff02975951d87da11763729e88fdead19c0
FlorenciaNieva <florencianieva930@gmail.com>
2023-12-03 15:10:49 -0300

    Incorporo prop complete a el componente Task

--- src/components/Main/ContainerLists.jsx
+++ src/components/Main/ContainerLists.jsx
@@ -5,7 +5,7 @@ export default function ContainerLists({ tasks, onDelete, setCompleted }) {
   return (
     <>
       {tasks.map((task) => (
-        <Task key={task.id} task={task.task} id={task.id} onDelete={onDelete} setCompleted={setCompleted} />
+        <Task key={task.id} task={task.task} id={task.id} complete={task.complete} onDelete={onDelete} setCompleted={setCompleted} />
       ))}
     </>
   );

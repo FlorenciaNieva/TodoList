81d61abf7578b31bd8c801a7998332d4a5b1b2fb
FlorenciaNieva <florencianieva930@gmail.com>
2023-12-05 18:21:40 -0300

    Paso props y los utilizo en taskForm

--- src/components/Main/ContainerForm.jsx
+++ src/components/Main/ContainerForm.jsx
@@ -3,12 +3,12 @@ import Task from './TaskForm'
 import Select from './SelectForm'
 import { Flex, Spacer, Box} from '@chakra-ui/react'
 
-export default function ContainerForm({ tasks, setTasks, filterTasks, selectedFilter }) {
+export default function ContainerForm({ tasks, setTasks, filterTasks, selectedFilter,originalTasks, setSelectedFilter }) {
   return (
     <>
       <Flex>
         <Box m='4' p='10' w='50%' border='1px' borderColor='#f6f1ff' borderRadius='15px' backdropFilter='auto' backdropBlur='8px'>
-          <Task tasks={tasks} setTasks={setTasks}/>
+          <Task tasks={tasks} setTasks={setTasks} originalTasks={originalTasks} setSelectedFilter={setSelectedFilter} />
         </Box>
         <Spacer />
         <Box m='4' p='10' w='50%' border='1px' borderColor='#f6f1ff' borderRadius='15px' backdropFilter='auto' backdropBlur='8px'>

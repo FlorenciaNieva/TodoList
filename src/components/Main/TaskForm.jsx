c7527274609190db1b7f186f10b5458c7776a899
FlorenciaNieva <florencianieva930@gmail.com>
2023-12-13 12:50:59 -0300

    Modificó formato de documento con prettier

--- src/components/Main/TaskForm.jsx
+++ src/components/Main/TaskForm.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import { useState } from 'react'
-import { FormControl, FormLabel, Text, Input, Button} from '@chakra-ui/react'
+import React from "react";
+import { useState } from "react";
+import { FormControl, FormLabel, Text, Input, Button } from "@chakra-ui/react";
 import { FaAngleRight } from "react-icons/fa";
 
-export default function TaskForm({ tasks, setTasks, originalTasks, setSelectedFilter }) {
-  const [inputValue, setInputValue] = useState('');
-  const [error, setError] = useState('');
+export default function TaskForm({
+  tasks,
+  setTasks,
+  originalTasks,
+  setSelectedFilter,
+}) {
+  const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState("");
 
   function validateTask(value) {
     if (!value) {
@@ -20,51 +25,53 @@ export default function TaskForm({ tasks, setTasks, originalTasks, setSelectedFi
       setError("");
     }
     return error;
-    }
+  }
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-        const newTask = {
-          id: self.crypto.randomUUID(),
-          task: inputValue,
-          complete: false
-        };
-        const updatedTasks = [...tasks, newTask];
-        setTasks(updatedTasks);
-        localStorage.setItem('tasks', JSON.stringify(updatedTasks));
-        setSelectedFilter('all');
-        setInputValue(''); 
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTask = {
+      id: self.crypto.randomUUID(),
+      task: inputValue,
+      complete: false,
+    };
+    const updatedTasks = [...tasks, newTask];
+    setTasks(updatedTasks);
+    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+    setSelectedFilter("all");
+    setInputValue("");
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-          <FormControl isRequired>
-            <FormLabel color='#f6f1ff'>Task</FormLabel>
-            <Input 
-              name='task' 
-              value={inputValue} 
-              onChange={(e) => {
-                setInputValue(e.target.value);
-                validateTask(e.target.value);
-              }} 
-              placeholder='Escribe una tarea ...' 
-              color='#f6f1ff' borderColor='gray.200' sx={{ '::placeholder': { color: '#f6f1ff' } }} 
-              focusBorderColor='#6e0acd' 
-              />
-            {!error ? null : <Text color='#f6f1ff'>{error}</Text>}
-          </FormControl>
-          <Button
-            mt={4}
-            rightIcon={<FaAngleRight />}
-            variant='outline'
-            type='submit'
-            color='#f6f1ff'
-            _hover={{
-              background: "#8e0acd",
-            }}
-          >
-            Add task
-          </Button>
-        </form>
-    ) 
+  return (
+    <form onSubmit={handleSubmit}>
+      <FormControl isRequired>
+        <FormLabel color="#f6f1ff">Task</FormLabel>
+        <Input
+          name="task"
+          value={inputValue}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+            validateTask(e.target.value);
+          }}
+          placeholder="Escribe una tarea ..."
+          color="#f6f1ff"
+          borderColor="gray.200"
+          sx={{ "::placeholder": { color: "#f6f1ff" } }}
+          focusBorderColor="#6e0acd"
+        />
+        {!error ? null : <Text color="#f6f1ff">{error}</Text>}
+      </FormControl>
+      <Button
+        mt={4}
+        rightIcon={<FaAngleRight />}
+        variant="outline"
+        type="submit"
+        color="#f6f1ff"
+        _hover={{
+          background: "#8e0acd",
+        }}
+      >
+        Add task
+      </Button>
+    </form>
+  );
 }
